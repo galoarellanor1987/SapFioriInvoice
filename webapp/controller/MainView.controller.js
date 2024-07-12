32b97205ab44286c9811e20d4c1746f8dfa98559
garellano <galo.arellano1987@gmail.com>
2024-07-12 21:56:51 +0000

    Commit con paises

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,6 +6,12 @@ function (Controller) {
 
     return Controller.extend("aktive.invoice.controller.MainView", {
         onInit: function () {
+            const oJsonModel=new sap.ui.model.json.JSONModel();
+            const oModel = new sap.ui.model.json.JSONModel(jQuery.sap.getModulePath("aktive.invoice.model", "/selectionScreenMenu.json"));
+            const oView=this.getView();
+          
+            oView.setModel(oModel, "selectionScreen");
+
 
         }
     });
