fee365871ef6c7ec2ee84513796cc6cc76be69db
garellano <galo.arellano1987@gmail.com>
2024-07-15 21:05:20 +0000

    Terminado

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -13,6 +13,38 @@ function (Controller) {
             oView.setModel(oModel, "selectionScreen");
 
 
+        },
+        onFilter: function (){
+                const oData = this.getView().getModel("selectionScreen").getData();
+                
+                let filters = [];
+                if (oData.Shipname !== "")
+                    {
+                        //alert(oData.ShipName);
+                        filters.push(new sap.ui.model.Filter("ShipName", sap.ui.model.FilterOperator.Contains, oData.ShipName));
+                    }
+                if (oData.CountryKey !== "")
+                    {
+                       // alert(oData.CountryKey); 
+                        filters.push(new sap.ui.model.Filter("Country", sap.ui.model.FilterOperator.EQ, oData.CountryKey));
+                    }
+
+                    const oList = this.getView().byId("InvoiceListId");
+                    const oBinding = oList.getBinding("items");
+                    oBinding.filter(filters);
+
+        },
+        onClearFilter: function()
+        {
+            const oModelSelScreen=this.getView().getModel("selectionScreen");
+            oModelSelScreen.setProperty("/CountryKey","");
+            oModelSelScreen.setProperty("/ShipName","");
+            
+            const oList = this.getView().byId("InvoiceListId");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter([]);
+
+
         }
     });
 });
